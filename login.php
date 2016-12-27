16a6e3195f87feaf8dd7ad5baa4918c1f391b0db
Mathias Van Sele <mathvans1@student.arteveldehs.be>
2016-12-27 18:10:08 +0100

    wip

--- login.php
+++ login.php
@@ -35,34 +35,37 @@ if(isset($_POST['login'])){
 
 <main>
     <?php echo $errMsg;?>
+    <div class="row"></div>
     <div class="row">
+        <div class="container">
 
-    <form role="form" class="col s12" method="POST" action="login.php" >
+    <form role="form" class="" method="POST" action="login.php" >
         <div class="row">
 
             <div class="container">
 
-                <div class="input-field col s6">
+                <div class="input-field col s10">
                     <i class="material-icons prefix">face</i>
                     <input name="username" id="icon_prefix" type="text" class="validate">
                     <label for="icon_prefix">Naam</label>
                 </div>
 
 
-                <div class="input-field col s6">
+                <div class="input-field col s10">
                     <i class="material-icons prefix">lock</i>
                     <input name="password" id="icon_psw" type="password" class="validate">
                     <label for="icon_psw">password</label>
                 </div>
 
-                <div class="col s12" >
-                    <button id="submit" class="btn waves-effect waves-light" type="submit" name="login">Login
+                <div class="col s8" >
+                    <button id="submit" class="btn waves-effect waves-light center-align" type="submit" name="login">Login
                         <i class="material-icons right">send</i>
                     </button>
 
                 </div>
             </div>
     </form>
+    </div>
     </div>
 
 
