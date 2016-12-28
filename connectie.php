b8266d9de6305eacfacd2106441e0caaa23d29d1
Mathias Van Sele <mathvans1@student.arteveldehs.be>
2016-12-28 15:31:51 +0100

    wip

--- connectie.php
+++ connectie.php
@@ -10,8 +10,8 @@ try{
     $db = new PDO($dns, $username, $pass);
 
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    /*echo "<div id='succes'>"."conncetion database succesful"."</div>";
-    echo '</br>';*/
+    echo "<div id='succes'>"."conncetion database succesful"."</div>";
+    echo '</br>';
 }catch(PDOException $e){
     echo "<div id=fout>". "<div id='fout_titel'>" . "You failed misserable piece of shit" . "</div>" . "</br>" . "Errormessage: ". $e . "</div>";
 };
