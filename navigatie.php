f13ef181bcb53d86e7dd566c15d662bdf9c311f1
Jonatan Moerman <jonatan.moerman@gmail.com>
2016-12-28 14:59:19 +0100

    add food & rest

--- navigatie.php
+++ navigatie.php
@@ -5,6 +5,8 @@ if(isset($_SESSION['username'])){
         'index.php' => 'Home',
         'addbar.php' => "Add Bar",
         'addbooze.php' => "Add Booze",
+        'addresto.php' => "Add Resto",
+        'addfood.php' => "Add Food",
         'bars.php' => "Check bars",
         'users.php' => "gebruikers",
 
