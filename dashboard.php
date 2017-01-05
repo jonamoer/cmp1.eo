b9babf20477f0a295182b31583121e3c900b9dcc
Jonatan Moerman <jonatan.moerman@student.arteveldehs.be>
2017-01-05 15:41:24 +0100

    wip

--- dashboard.php
+++ dashboard.php
@@ -19,7 +19,7 @@ require_once 'navigatie.php';
             echo "<h2>Gegevens:</h2>";
             echo "<div><h5>Naam: {$row["username"]}</h5></div>";
             echo "<div><h5>E-mail: {$row["email"]}</h5></div>";
-            echo "<a href='edit_user.php?edite_id={$row['id']}'><i class='material-icons right'>edite</i>Edite</a>";
+            echo "<a class='btn waves-effect waves-light' href='edit_user.php?edite_id={$row['id']}'><i class='material-icons right'>edite</i>Edite</a>";
             echo "</div>";
         }?>
 </div>
